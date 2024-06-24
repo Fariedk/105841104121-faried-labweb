0be808d2bbd81aac288e297fe8c77e6562f79a5f
Fariedk <105841104121@student.unismuh.ac.id>
2024-06-24 14:40:12 +0800

    Custom Komponen dan Text Input

--- App.js
+++ App.js
@@ -1,26 +1,37 @@
-import { StyleSheet, Text, View, Image } from 'react-native'
+import { StyleSheet, Text, View, Image, TextInput } from 'react-native'
 import React from 'react'
 import logo from './assets/Sabo1.png'
 
-const App = () => {
+const ButtonCustom = ({text, color}) => {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text style={{ fontSize: 30, marginBottom: 50 }}>Welcome!</Text>
-      <Image
-        source={logo}
-        style={{ width: 200, height: 150, marginBottom: 100 }}
-        />
-        
-      <View style={{ flexDirection: 'row', justifyContent: 'space-between', width: '80%'}}>
-        <View style={{ width: '45%', height: 50, backgroundColor: 'red',borderRadius:15, alignItems:'center', justifyContent:'center'}}>
-          <Text style={{color:'white'}}>Sign Up</Text>
-        </View>
-        <View style={{ width: '45%', height: 50, backgroundColor: 'green', borderRadius:15, alignItems:'center', justifyContent:'center'}}>
-          <Text style={{color:'white'}}>Login</Text>
-        </View>
-      </View>
+    <View style={{ backgroundColor: color, width: 250, height: 100, borderRadius: 15, justifyContent: 'center', marginBottom: 10 }}>
+      <Text style={{ textAlign: 'center', color: 'black', fontSize: 25, fontWeight: 'bold' }}>
+          {text}
+        </Text>
     </View>
   )
 }
+
+const TextInputCustom = ({placeholder, color, typeKeyboard}) => {
+  return (
+    <TextInput
+      placeholder={placeholder}
+      keyboardType={typeKeyboard}
+      style={{ width: 250, height: 50, borderColor: color, borderWidth: 3, borderRadius: 15, marginBottom: 10, paddingLeft: 10 }}
+    />
+  )
+}
+const App = () => {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <View>
+        <TextInputCustom placeholder='Enter your name' color='red' />
+        <TextInputCustom placeholder='Enter your age' color='blue' typeKeyboard='numeric' />
+      </View>
+      
+    </View>
+  )
+}
+
 
 export default App
